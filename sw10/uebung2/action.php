5b37697ab79d903738fd6360a19e4d372b130afc
flawas <32436687+flawas@users.noreply.github.com>
2023-04-24 20:41:09 +0200

    SW10: Task2 Correction output

--- sw10/uebung2/action.php
+++ sw10/uebung2/action.php
@@ -10,9 +10,9 @@
     <?php
     // TODO: Prüfen, ob Parameters 'name' gesetzt ist (ersetzen Sie true)
     if (isset($_POST['name']))  {
-        
+        $name = $_POST['name'];
         // TODO: Ausgabe des Parameters 'name'
-        echo "Hello ";
+        echo "Hello '$name";
         
     } else {
         echo "<p>Parameter 'name' nicht gesetzt</p>";
