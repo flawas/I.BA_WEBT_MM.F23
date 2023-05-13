e1a0568513ab22718ddaec15da806943c17d7198
flawas <32436687+flawas@users.noreply.github.com>
2023-05-13 14:14:52 +0200

    MEP: Added Accordeon

--- WEBT_FS23_WASER_FLAVIO/javascript.js
+++ WEBT_FS23_WASER_FLAVIO/javascript.js
@@ -22,3 +22,12 @@ function carousel() {
 
 // Get actual year
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+function accordeon(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+}
