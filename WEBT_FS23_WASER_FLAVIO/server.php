900a0784c0014a9da3fd8cc024557b282ab48587
flawas <32436687+flawas@users.noreply.github.com>
2023-05-13 15:02:58 +0200

    MEP: Adding tel and text to form

--- WEBT_FS23_WASER_FLAVIO/server.php
+++ WEBT_FS23_WASER_FLAVIO/server.php
@@ -1,5 +1,33 @@
 <?php
 
+    // Datenbank Server
+
+
     // Validierung und Speicherung
-    
+    function validate() {
+        if (!isset($_POST['email'])) {
+            echo "<p>Parameter 'E-Mail' ist benötigt</p>";
+            return false;
+        }
+        if (!isset($_POST['vorname'])) {
+            echo "<p>Parameter 'Vorname' ist benötigt</p>";
+            return false;
+        }
+        if (!isset($_POST['nachname'])) {
+            echo "<p>Parameter 'Nachname' ist benötigt</p>";
+            return false;
+        }
+        if (!isset($_POST['Mannschaft'])) {
+            echo "<p>Parameter 'Mannschaft' ist benötigt</p>";
+            return false;
+        }
+        if (!isset($_POST['geburtstag'])) {
+            echo "<p>Parameter 'geburtstag' ist benötigt</p>";
+            return false;
+        }
+        return true;
+    }
+
+    if()
+
 ?>
