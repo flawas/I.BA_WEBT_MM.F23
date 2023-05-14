7dd98f20aa171232d052f554d4308ae66bd1b493
flawas <32436687+flawas@users.noreply.github.com>
2023-05-14 15:43:40 +0200

    MEP: Server html

--- WEBT_FS23_WASER_FLAVIO/server.php
+++ WEBT_FS23_WASER_FLAVIO/server.php
@@ -16,106 +16,120 @@
         <a href="index.html" class="w3-bar-item w3-button">Home</a>
     </nav>
     <div class="w3-container">
-    <?php
-        session_start();
-        function validate_parameters() {
-            if (!isset($_POST['vorname'])) {
-                echo "<p>Parameter 'Vorname' ist benötigt</p>";
-                return false;
-            }
-            if (!isset($_POST['nachname'])) {
-                echo "<p>Parameter 'Nachname' ist benötigt</p>";
-                return false;
-            }
-            if (!isset($_POST['mannschaft'])) {
-                echo "<p>Parameter 'Mannschaft' ist benötigt</p>";
-                return false;
-            }
-            if (!isset($_POST['email'])) {
-                echo "<p>Parameter 'E-Mail' ist benötigt</p>";
-                return false;
-            }
-            if (!isset($_POST['geburtstag'])) {
-                echo "<p>Parameter 'geburtstag' ist benötigt</p>";
-                return false;
-            }
-            $_SESSION['vorname'] = $_POST['vorname'];
-            $_SESSION['nachname'] = $_POST['nachname'];
-            $_SESSION['mannschaft'] = $_POST['mannschaft'];
-            $_SESSION['email'] = $_POST['email'];
-            $_SESSION['geburtstag'] = $_POST['geburtstag'];
-            $_SESSION['bemerkungen'] = $_POST['bemerkungen'];
-            
-            return true;
-        }
-
-        function alert($level, $title, $message) {
-            if($level == "success") {
-                echo "
-                <div class='w3-panel w3-green'>
-                    <h3>$title</h3>
-                    <p>$message</p>
-                </div>";
-            }
-            if($level == "info") {
-                echo "
-                <div class='w3-panel w3-blue'>
-                    <h3>$title</h3>
-                    <p>$message</p>
-                </div>";
-            }
-            if($level == "error") {
-                echo "
-                <div class='w3-panel w3-red'>
-                    <h3>$title</h3>
-                    <p>$message</p>
-                </div>";
-            }
-        }
-
-        
-        if(validate_parameters()){
-            // Create connection
-            $conn = mysqli_connect("flawasch.mysql.db.internal", "flawasch_hslu", "UbXGbs1kvLPcWdfULoxx", "flawasch_hslu");
-            if (!$conn) { 
-                echo "<p>Database connection failed</p>";
-            }         
-            // Check connection
-            if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-            }
-
-            $vorname = $_SESSION['vorname'];
-            $nachname = $_SESSION['nachname'];
-            $mannschaft = $_SESSION['mannschaft'];
-            $email = $_SESSION['email'];
-            $geburtstag = $_SESSION['geburtstag'];
-            $bemerkungen = $_SESSION['bemerkungen'];
-
-            $sql = "INSERT INTO webt_mep (vorname, nachname, mannschaft, email, bemerkungen, geburtstag)
-                VALUES ('$vorname', '$nachname', '$mannschaft', '$email', '$bemerkungen', '$geburtstag')";
-
-            if($_COOKIE['angemeldet'] == true){
-                alert("info", "Nicht übermittlet", "Du bist bereits angemeldet");
-            } else {
-                if ($conn->query($sql) === TRUE) {
-                    alert("success", "Übermittelt", "Die Anmeldung wurde erfolgreich gespeichert.");
-                    setcookie("angemeldet", true);
-                    $res = true;
-                } else {
-                    alert("error", "Fehler", "Die Anmeldung konnte nicht verarbeitet werden.");
-                    echo "Error: " . $sql . "<br>" . $conn->error;
+        <?php
+            session_start();
+            function validate_parameters() {
+                if (!isset($_POST['vorname'])) {
+                    echo "<p>Parameter 'Vorname' ist benötigt</p>";
+                    return false;
                 }
-        
-        
-                if ($res) {
-                    echo "<p>Besten Dank für Ihre Anmeldung.</p>";
-                } else {
-                    echo "<p>Ein Fehler ist aufgetreten.</p>";
-                }  
+                if (!isset($_POST['nachname'])) {
+                    echo "<p>Parameter 'Nachname' ist benötigt</p>";
+                    return false;
+                }
+                if (!isset($_POST['mannschaft'])) {
+                    echo "<p>Parameter 'Mannschaft' ist benötigt</p>";
+                    return false;
+                }
+                if (!isset($_POST['email'])) {
+                    echo "<p>Parameter 'E-Mail' ist benötigt</p>";
+                    return false;
+                }
+                if (!isset($_POST['geburtstag'])) {
+                    echo "<p>Parameter 'geburtstag' ist benötigt</p>";
+                    return false;
+                }
+                $_SESSION['vorname'] = $_POST['vorname'];
+                $_SESSION['nachname'] = $_POST['nachname'];
+                $_SESSION['mannschaft'] = $_POST['mannschaft'];
+                $_SESSION['email'] = $_POST['email'];
+                $_SESSION['geburtstag'] = $_POST['geburtstag'];
+                $_SESSION['bemerkungen'] = $_POST['bemerkungen'];
+
+                return true;
             }
 
-            mysqli_close($conn);
-        }
+            function alert($level, $title, $message) {
+                if($level == "success") {
+                    echo "
+                    <div class='w3-panel w3-green'>
+                        <h3>$title</h3>
+                        <p>$message</p>
+                    </div>";
+                }
+                if($level == "info") {
+                    echo "
+                    <div class='w3-panel w3-blue'>
+                        <h3>$title</h3>
+                        <p>$message</p>
+                    </div>";
+                }
+                if($level == "error") {
+                    echo "
+                    <div class='w3-panel w3-red'>
+                        <h3>$title</h3>
+                        <p>$message</p>
+                    </div>";
+                }
+            }
+            
+            if(validate_parameters()){
+                // Create connection
+                $conn = mysqli_connect("flawasch.mysql.db.internal", "flawasch_hslu", "UbXGbs1kvLPcWdfULoxx", "flawasch_hslu");
+                if (!$conn) { 
+                    echo "<p>Database connection failed</p>";
+                }         
+                // Check connection
+                if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+                }
 
-    ?>
+                $vorname = $_SESSION['vorname'];
+                $nachname = $_SESSION['nachname'];
+                $mannschaft = $_SESSION['mannschaft'];
+                $email = $_SESSION['email'];
+                $geburtstag = $_SESSION['geburtstag'];
+                $bemerkungen = $_SESSION['bemerkungen'];
+
+                $sql = "INSERT INTO webt_mep (vorname, nachname, mannschaft, email, bemerkungen, geburtstag)
+                    VALUES ('$vorname', '$nachname', '$mannschaft', '$email', '$bemerkungen', '$geburtstag')";
+
+                if($_COOKIE['angemeldet'] == true){
+                    alert("info", "Anmeldung nicht übermittlet", "Du bist bereits für das Lager angemeldet");
+                } else {
+                    if ($conn->query($sql) === TRUE) {
+                        alert("success", "Übermittelt", "Die Anmeldung wurde erfolgreich gespeichert.");
+                        setcookie("angemeldet", true);
+                        $res = true;
+                    } else {
+                        alert("error", "Fehler", "Die Anmeldung konnte nicht verarbeitet werden.");
+                        echo "Error: " . $sql . "<br>" . $conn->error;
+                    }
+            
+            
+                    if ($res) {
+                        echo "<p>Besten Dank für Ihre Anmeldung.</p>";
+                    } else {
+                        echo "<p>Ein Fehler ist aufgetreten.</p>";
+                    }  
+                }
+
+                mysqli_close($conn);
+            }
+
+        ?>
+
+        <h1>Übermittelte Daten</h1>
+        <ul class="w3-ul">
+            <li><?php echo $_SESSION['vorname']; ?></li>
+            <li><?php echo $_SESSION['nachname']; ?></li>
+            <li><?php echo $_SESSION['mannschaft']; ?></li>
+            <li><?php echo $_SESSION['email']; ?></li>
+            <li><?php echo $_SESSION['geburstag']; ?></li>
+            <li><?php echo $_SESSION['bemerkungen']; ?></li>
+        </ul>
+    </div>
+</body>
+<footer class="myclass-colorinverse myclass-footer">
+    <h6>© <span id="year"></span> HC Weggis-Küssnacht by Flavio Waser</h6>
+</footer>
