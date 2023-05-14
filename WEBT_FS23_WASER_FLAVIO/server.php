60bb84b325b9dd05664317c3ed5656bf3070080e
flawas <32436687+flawas@users.noreply.github.com>
2023-05-14 14:51:13 +0200

    MEP: DB connect and save data

--- WEBT_FS23_WASER_FLAVIO/server.php
+++ WEBT_FS23_WASER_FLAVIO/server.php
@@ -1,13 +1,18 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>HCWK Lageranmeldung</title>
+    <link rel="icon" type="image/png" href="img/favicon.jpg"> 
+    <link rel="stylesheet" href="sylesheet.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
 <?php
 
-    // Datenbank Server
-    $server = "flawasch.mysql.db.internal";
-    $username = "flawasch_hslu";
-    $password = "UbXGbs1kvLPcWdfULoxx";
-    $database = "flawasch_hslu";
+    session_start();
 
-    // Validierung und Speicherung
-    function validate() {
+    function validate_parameters() {
         if (!isset($_POST['vorname'])) {
             echo "<p>Parameter 'Vorname' ist benötigt</p>";
             return false;
@@ -28,40 +33,58 @@
             echo "<p>Parameter 'geburtstag' ist benötigt</p>";
             return false;
         }
+
+        $_SESSION['vorname'] = $_POST['vorname'];
+        $_SESSION['nachname'] = $_POST['nachname'];
+        $_SESSION['mannschaft'] = $_POST['mannschaft'];
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['geburtstag'] = $_POST['geburtstag'];
+        $_SESSION['bemerkungen'] = $_POST['bemerkungen'];
+
         return true;
     }
 
-    if(validate()) {
-        $conn = mysqli_connect($server, $username, $password, $database);
+
+    
+    if(validate_parameters()){
+        // Create connection
+        $conn = mysqli_connect("flawasch.mysql.db.internal", "flawasch_hslu", "UbXGbs1kvLPcWdfULoxx", "flawasch_hslu");
         if (!$conn) { 
             echo "<p>Database connection failed</p>";
+        }         
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected successfully";
+
+        $vorname = $_SESSION['vorname'];
+        $nachname = $_SESSION['nachname'];
+        $mannschaft = $_SESSION['mannschaft'];
+        $email = $_SESSION['email'];
+        $geburtstag = $_SESSION['geburtstag'];
+        $bemerkungen = $_SESSION['bemerkungen'];
+
+        echo $geburstag;
+
+        $sql = "INSERT INTO webt_mep (vorname, nachname, mannschaft, email, bemerkungen, geburtstag)
+            VALUES ('$vorname', '$nachname', '$mannschaft', '$email', '$bemerkungen', '$geburtstag')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+            $res = true;
         } else {
-            $vorname = $_POST['vorname'];
-            $nachname = $_POST['nachname'];
-            $mannschaft = $_POST['mannschaft'];
-            $geburtstag = $_POST['geburtstag'];
-            $email = $_POST['email'];
-            $bemerkungen = $_POST['bemerkungen'];
-    
-            $sql = "INSERT INTO webt_mep (vorname, nachname, mannschaft, geburstag, email, bemerkungen)
-                VALUES ($vorname, $nachname, $mannschaft, $geburtstag, $email, $bemerkungen)";
-    
-            if ($conn->query($sql) === TRUE) {
-                echo "New record created successfully";
-                $res = true;
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-    
-            
-            if ($res) {
-                echo "<p>Besten Dank für Ihre Anmeldung.</p>";
-            } else {
-                echo "<p>Ein Fehler ist aufgetreten.</p>";
-            }
-            mysqli_close($conn);        
+            echo "Error: " . $sql . "<br>" . $conn->error;
         }
 
+
+        if ($res) {
+            echo "<p>Besten Dank für Ihre Anmeldung.</p>";
+        } else {
+            echo "<p>Ein Fehler ist aufgetreten.</p>";
+        }  
+
+        mysqli_close($conn);
     }
 
 ?>
