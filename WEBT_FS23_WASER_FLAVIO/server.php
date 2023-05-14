e11178c6af37114e1cdbdda8058db778a8e3cb52
flawas <32436687+flawas@users.noreply.github.com>
2023-05-14 16:28:24 +0200

    MEP: Table correction

--- WEBT_FS23_WASER_FLAVIO/server.php
+++ WEBT_FS23_WASER_FLAVIO/server.php
@@ -134,7 +134,7 @@
 
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
-                echo "<tr><td>" . $row["id"]. "</td><td>" . $row["vorname"]. " " . $row["nachname"]. "</td><td>" . $row["timestamp"]. "</td></tr>";
+                echo "<tr><td>" . $row["id"]. "</td><td>" . $row["vorname"]. "</td><td>" . $row["nachname"]. "</td><td>" . $row["timestamp"]. "</td></tr>";
             }
             echo "</table>";
         } else {
